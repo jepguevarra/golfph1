a15eca0e415d1cd8411c2ea2a75a6358dc19cd8c
jepguevarra <109466623+jepguevarra@users.noreply.github.com>
2025-10-11 15:06:39 +0800

    Update route.js

--- app/api/odoo/route.js
+++ app/api/odoo/route.js
@@ -1,6 +1,6 @@
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // or your specific domain
+  res.setHeader("Access-Control-Allow-Origin", "https://appsumo55348.directoryup.com/"); // or your specific domain
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
